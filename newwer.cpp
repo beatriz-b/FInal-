c440477ae66c231678b5eb05434cdef57d00596e
beatriz-b <54721326+beatriz-b@users.noreply.github.com>
2019-12-12 20:21:40 -0800

    Update newwer.cpp

--- newwer.cpp
+++ newwer.cpp
@@ -1,5 +1,5 @@
-/ FINAL.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// FINAL.cpp : This file contains the 'main' function.Program execution begins and ends there.
+//https://www.youtube.com/watch?v=nD_huaZ6XjM
 
 #include "pch.h"
 #include <iostream>
@@ -48,9 +48,72 @@ void home() {
 	cout << "_________________________________________\n";
 }
 
-int levelOne(int outcomes[], vector<string> attributes) 
+
+
+int levelOne(int outcomes[]) {
+
+	cout << "I should really be heading out, its a long drive so I need to go "
+		"ASAP" << endl;
+	cout << "But my rooms a mess, I don't want to give the maid too much of a "
+		"hard time, should I make my bed first?" << endl;
+	cout << "Press 'y' to make bed" << endl;
+	cout << "Press 'n' to get on with your day, YOU HAVE NO TIME TO WASTE" << endl;
+
+	int response;
+	cin >> response;
+
+	return response;
+	}
+
+int levelTwo(int outcomes[]) {
+
+	int previousChoice = outcomes[0];
+
+	if (previousChoice == 'y' || previousChoice == 'Y') {
+
+		cout << "While making the bed something goes flying out" << endl;
+		cout << "Wait whats that?? AWE YEAH WHAT!! I just bagged $150 out of no"
+			" where :D. endl;endl; Oh wait, this is mine" << endl;
+
+		int response;
+		cin >> response;
+	}
+	else if (previousChoice == 'n' || previousChoice == 'N') {
+
+		cout << " No I probably shouldn't I need to go now. endl; Lets see I"
+			" have my car keys, wallet, no clothes to pack for the one day"
+			" trip. I have.... some random persons pants in my room. okay." << endl;
+
+		cout << " Press 3 to leave out the door" << endl;
+		cout << " Press 4 to double check the room" << endl;
+
+		int response;
+		cin >> response;
+	}
+	return response;
+}
 
 int main() {
-	
+
+	const int LEVELS = 10;
+	int outcomes[LEVELS];
+
+	// Introduction should go up here and then cut to new screen when user "press ENTER to continue" 
+	char response;
+	room();
+	cout << "How late it is? Wheres my phone??" << endl;
+	cin >> response;
+
+	//make sure phone is useable
+
+	while (response == 'y') {
+		//call our level functions 
+		outcomes[0] = levelOne(outcomes);
+
+		levelTwo(outcomes);
+
+	}
+
+
 	return 0;
 }
