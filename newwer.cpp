663284f248e6058610dcc9524ba1b8096bf67c85
beatriz-b <54721326+beatriz-b@users.noreply.github.com>
2019-12-12 18:34:47 -0800

    Create newwer.cpp

---
+++ newwer.cpp
@@ -0,0 +1,56 @@
+/ FINAL.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void room() {
+	cout << "__________________________________________________________________\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "___________________(shoe)_______________\n";
+}
+
+void hallway() {
+	cout << "----------------------------------------\n";
+	cout << "\n";
+	cout << "                         0\n";
+	cout << "                         I\n";
+	cout << "                        I I\n";
+	cout << "_________________________________________\n";
+}
+
+void reception() {
+	cout << "----------------------------------------\n";
+	cout << "\n";
+	cout << "                         0\n";
+	cout << "                         I\n";
+	cout << "                        I I\n";
+	cout << "_________________________________________\n";
+}
+
+void home() {
+	cout << "----------------------------------------\n";
+	cout << "\n";
+	cout << "                         0\n";
+	cout << "                         I\n";
+	cout << "                        I I\n";
+	cout << "_________________________________________\n";
+}
+
+int levelOne(int outcomes[], vector<string> attributes) 
+
+int main() {
+	
+	return 0;
+}
